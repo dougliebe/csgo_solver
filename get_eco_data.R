da03c858070501baee7a593b615cfe34a996b9c1
Doug Leibe <Doug.Liebe@hollywoodparkca.com>
2021-07-31 11:00:04 -0700

    more work on figuring out what a team actually had to spend

--- get_eco_data.R
+++ get_eco_data.R
@@ -1,7 +1,7 @@
 ### eco decisions
 library(tidyverse)
 
-data <- read_csv("D:/hltvxDK/economy.csv")
+data <- read_csv(here::here("economy.csv"))
 head(data)
 
 ## reshape data into long form
@@ -82,6 +82,50 @@ data %>%
 
 full_data <- team_1_data %>% 
   bind_rows(team_2_data)
+
+## need at least three things to predict available cash
+## loss streak, win streak, min money for those things
+ff = function(x)
+{
+  cs = cumsum(x)
+  cs - cummax((x == 0) * cs)
+}
+
+full_data %>% 
+  filter(!is.na(spent)) %>% 
+  group_by(match_id, event_id, map, team, side) %>% 
+  mutate(loss_score = ifelse(cumsum(ifelse(win == 0, 1, -1))),
+         loss_streak = cumsum(win),
+         win_streak = cumsum(win == 1)) %>% 
+  group_by(loss_streak, .add = T) %>% 
+  mutate(current_loss_streak = cumsum(win==0)) %>% 
+  group_by(match_id, event_id, map, team, side, win_streak) %>% 
+  mutate(current_win_streak = cumsum(win)) %>% 
+  ungroup() %>% 
+  slice(1:100) %>% view
+
+## need to see how many rounds in the future the spend is correlated
+## could look at buy diff and how it corrs, probably better
+.temp <- tibble()
+
+for(i in 1:8) {
+  full_data %>% 
+    filter(!is.na(spent)) %>% 
+    group_by(match_id, event_id, map, team, side) %>% 
+    mutate(spend_diff = spent - opp_spent,
+           next_diff = lead(spent,i)) %>% 
+    filter(!is.na(next_diff), spend_diff < 0 ) %>% 
+    ungroup() %>% 
+    sample_n(10000) %>% 
+    summarise(m = cor(spent, next_diff)) %>% 
+    bind_rows(.temp) ->
+    .temp
+}
+.temp %>% 
+  mutate(rounds = 8:1) %>% 
+  ggplot(aes(rounds, m))+
+  geom_line()
+  
 
 full_data %>% 
   group_by(match_id, event_id, map, team) %>% 
